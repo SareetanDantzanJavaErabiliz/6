a73864217dbbb1218e95bff1ed34ee5490f1edc5
maiderHuarte <46961582+maiderHuarte@users.noreply.github.com>
2021-11-22 13:20:35 +0100

    Create IzakiBizidunak.java

---
+++ Berridazketa/IzakiBizidunak.java
@@ -0,0 +1,13 @@
+
+
+/**
+ *
+ * Egileak: Iñaki Goirizelaia eta Maider Huarte. UPV/EHU.
+ */
+public class IzakiBizidunak {
+    public void mugitu( ) {
+        System.out.println( "Izaki bizidunak mugitzen dira" );
+    }
+    
+}
+
